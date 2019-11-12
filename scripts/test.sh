19651ba6bd0f14acde70326293c5049340b55f1d
Lucemia Chen <lucemia@gmail.com>
2019-11-12 21:47:05 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,10 +5,11 @@ set -ev
 
 cd /home
 
-mkdir -p /workspace/tmp
 mkdir -p ./tmp
 mkdir -p /workspace/out
 
 node test_1.js > /workspace/out/1.log
 node test_2.js > /workspace/out/2.log
-cp ./tmp/* /workspace/tmp
+
+apt install ffmpeg
+ffmpeg -i ./tmp/%d.jpg /workspace/out/2.mp4
