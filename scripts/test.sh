dded600fbac2ac35fd8cf911c8df591a87fedb9e
Lucemia Chen <lucemia@gmail.com>
2019-11-12 21:38:23 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,9 @@ set -ev
 cd /home
 
 mkdir -p /workspace/tmp
+mkdir -p ./tmp
 mkdir -p /workspace/out
 
 node test_1.js > /workspace/out/1.log
 node test_2.js > /workspace/out/2.log
+cp ./tmp/* /workspace/tmp
