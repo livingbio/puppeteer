c623dd9658b14eff93f5a0a0b542f894600a3195
Lucemia Chen <lucemia@gmail.com>
2019-11-12 22:05:38 +0800

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,5 +11,5 @@ mkdir -p /workspace/out
 node test_1.js > /workspace/out/1.log
 node test_2.js > /workspace/out/2.log
 
-apt install ffmpeg
+apt install -y ffmpeg
 ffmpeg -i ./tmp/%d.jpg /workspace/out/2.mp4
